1bd8d6b7f110b99f3b86f042ca40c4cedaf84962
Praise-G <123006857+Praise-G@users.noreply.github.com>
2023-04-22 19:58:07 +0200

    Update ForgotPassword.js
    Now includes the Forgot password backend function utilising firebase sdks.

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -2,11 +2,40 @@ import React from "react"
 import {Link} from "react-router-dom"
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import firebase from "firebase/app";
+import "firebase/auth";
+
 
 /*lastest commit*/
 
 const ForgotPassword = () => {
-return (
+
+    // the useState requires you to disable-next-line no-undef
+    const [email, setEmail] = useState("");
+
+    // utilizes firebases password reset email protocol
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        firebase
+            .auth()
+            .sendPasswordResetEmail(email)
+            .then(() => {
+                // Password reset email sent successfully
+                alert("Password reset email sent!");
+            })
+            .catch((error) => {
+                // An error occurred
+                console.error(error);
+                alert("Error sending password reset email");
+            });
+    };
+        
+    // this takes in the email input
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    return (
         <>
             <Meta title={"Forgot Password"}/>
             <BreadCrumb title="Forgot Password"/>
@@ -18,14 +47,18 @@ return (
                             <p className="text-center mt-2 mb-3">
                                 We will send you an email to reset your password
                             </p>
-                            <form action="" className="d-flex flex-column gap-15">
-                                <div>
-                                    <input type="email"
-                                           name ="email"
-                                           placeholder="Email"
-                                           className="form-control"
-                                    />
-                                </div>
+                            <form
+                                onSubmit={handleFormSubmit} // passes through the form from firebase protocol
+                                className="d-flex flex-column gap-15"
+                            >
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email"
+                                    className="form-control"
+                                    value={email}
+                                    onChange={handleEmailChange} //takes the email that has been entered and passes it through the firebase protocol
+                                />
                                 <div>
                                     <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
                                         <button className="button border-0" type="submit">
