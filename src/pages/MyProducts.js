5e510626c6ad8828b9b2142a115e47a4171596eb
neo-nkosi <127707311+neo-nkosi@users.noreply.github.com>
2023-04-23 16:52:54 +0200

    Update MyProducts.js
    fixed myProduct.js

--- src/pages/MyProducts.js
+++ src/pages/MyProducts.js
@@ -4,19 +4,13 @@ import Meta from '../components/Meta';
 import BreadCrumb from '../components/BreadCrumb';
 import SellerProductCard from '../components/SellerProductCard';
 import { Link } from 'react-router-dom';
-import {db} from "./firebase";
-import React, { useState } from 'react'
-import  Meta  from '../components/Meta'
-import BreadCrumb from '../components/BreadCrumb'
-import SellerProductCard from '../components/SellerProductCard'
-import { Link } from 'react-router-dom'
-import AddProductModal from '../components/AddProductModal'
-import UpdateProductModal from '../components/UpdateProductModal'
+import { db } from "./firebase";
+import AddProductModal from '../components/AddProductModal';
+import UpdateProductModal from '../components/UpdateProductModal';
 
-
-export const MyProducts = () => {
-    const [isAddOpen, setIsAddOpen] = useState(false)
-    const [isUpdateOpen, setIsUpdateOpen] = useState(false)
+const MyProducts = () => {
+    const [isAddOpen, setIsAddOpen] = useState(false);
+    const [isUpdateOpen, setIsUpdateOpen] = useState(false);
     const grid = 12;
 
     const [products, setProducts] = useState([]);
@@ -29,7 +23,7 @@ export const MyProducts = () => {
         fetchData();
     }, []);
 
-    console.log(products)
+    console.log(products);
 
     return (
         <>
@@ -58,7 +52,7 @@ export const MyProducts = () => {
                                     </select>
                                 </div>
                                 <div className="d-flex align-items-center gap-5">
-                                    <Link className="button">Add Product</Link>
+                                    <Link className="button" onClick={() => setIsAddOpen(true)}>Add Product</Link>
                                 </div>
                                 <div className="d-flex align-items-center gap-10">
                                     <p className="totalproducts">21 products</p>
@@ -77,53 +71,19 @@ export const MyProducts = () => {
                                         productDescription={product.description}
                                         productPrice={product.price}
                                         productStock={product.stock || 'Not available'}
+                                        editOnClick={() => setIsUpdateOpen(true)}
                                     />
                                 ))}
                             </div>
                         </div>
                     </div>
-
-  return (
-    <>
-        <Meta title={"My Products"}/>
-        <BreadCrumb title="My Products" />    
-        <div className="store-wrapper home-wrapper-2 py-5">
-            <div className='row'>
-                <div className='col-9'>
-                    <div className='filter-sort-grid mb-4'>
-                        <div className='d-flex justify-content-between align-items-center'>
-                            <div className='d-flex align-items-center gap -10'>
-                                <p className='mb-0 d-block' style={{"width":"100px"}}>Sort By:</p>
-                                <select name='' className='form-control form-select' id=''>
-                                    <option value="manual">Featured</option>
-                                    <option value="best-selling" selected="selected">Best Selling</option>
-                                    <option value="title-ascending">Alaphabetically, A-Z</option>
-                                    <option value="title-descending">Alaphabetically, Z-A</option>
-                                    <option value="price-ascending">Price, low to high</option>
-                                    <option value="price-descending">Price, high to low</option>
-                                    <option value="created-ascending">Date, old to new</option>
-                                    <option value="created-descending">Date, new to old</option>
-                                </select>
-                            </div>
-                            <div className='d-flex align-items-center gap-5'>
-                                <Link className="button" onClick={() => setIsAddOpen(true)}>Add Product</Link>
-                            </div>
-                            <div className='d-flex align-items-center gap-10'>
-                                <p className='totalproducts'>21 products</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='products-list pb-5'>       
-                        <div className='d-flex gap-10 flex-wrap'>
-                            <SellerProductCard grid={grid} editOnClick={() => setIsUpdateOpen(true)} />
-                            <AddProductModal open={isAddOpen} onClose={() => setIsAddOpen(false)}></AddProductModal>
-                            <UpdateProductModal open={isUpdateOpen} onClose={() => setIsUpdateOpen(false)}></UpdateProductModal>
-                        </div>          
-                    </div>
                 </div>
             </div>
+            <AddProductModal open={isAddOpen} onClose={() => setIsAddOpen(false)} />
+            <UpdateProductModal open={isUpdateOpen} onClose={() => setIsUpdateOpen(false)} />
         </>
     );
 };
 
-export default MyProducts;
+export default MyProducts
+
