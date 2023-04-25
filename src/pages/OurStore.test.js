f4459fa82223fff2889695399a84735a222a09d2
Praise-G <123006857+Praise-G@users.noreply.github.com>
2023-04-26 00:25:48 +0200

    Added more Unit Tests
    Added Unit Tests for OurStore page

---
+++ src/pages/OurStore.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import OurStore from './OurStore';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('OurStore Component', () => {
+    test('renders page title and breadcrumb', () => {
+        render(
+            <BrowserRouter>
+                <OurStore />
+            </BrowserRouter>
+        );
+
+        const ourStoreRegex = new RegExp('Our Store', 'i');
+        const pageTitle = screen.getByText(ourStoreRegex);
+        const breadcrumb = screen.getAllByText(ourStoreRegex);
+
+        expect(pageTitle).toBeInTheDocument();
+        expect(breadcrumb.length).toBeGreaterThan(0);
+    });
+
+    test('renders filter and sort section', () => {
+        render(
+            <BrowserRouter>
+                <OurStore />
+            </BrowserRouter>
+        );
+
+        const sortByText = screen.getByText('Sort By');
+        const selectElement = screen.getByRole('combobox');
+        const totalProductsText = screen.getByText(/Total Products:/i);
+
+        expect(sortByText).toBeInTheDocument();
+        expect(selectElement).toBeInTheDocument();
+        expect(totalProductsText).toBeInTheDocument();
+    });
+
+});
