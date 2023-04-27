1bf51f46ab475334366deb311eed51cc1635343d
neo-nkosi <127707311+neo-nkosi@users.noreply.github.com>
2023-04-27 09:59:57 +0200

    Added 3 tests to ProductCard.test.js

---
+++ src/components/ProductCard.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+import ProductCard from "./ProductCard";
+
+const defaultProps = {
+    grid: "col-3",
+    productImage: "test-image.jpg",
+    brand: "Test Brand",
+    productName: "Test Product",
+    productDescription: "Test Description",
+    productPrice: "100",
+    productStock: "10",
+};
+
+describe("ProductCard Component", () => {
+    it("renders the product card with given props", () => {
+        render(
+            <MemoryRouter>
+                <ProductCard {...defaultProps} />
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText(defaultProps.brand)).toBeInTheDocument();
+        expect(screen.getByText(defaultProps.productName)).toBeInTheDocument();
+        expect(screen.getByText(`R ${defaultProps.productPrice}`)).toBeInTheDocument();
+    });
+
+    it("renders the product image with correct src and alt attributes", () => {
+        render(
+            <MemoryRouter>
+                <ProductCard {...defaultProps} />
+            </MemoryRouter>
+        );
+        const productImageElement = screen.getByAltText(defaultProps.productName);
+        expect(productImageElement).toBeInTheDocument();
+        expect(productImageElement).toHaveAttribute("src", defaultProps.productImage);
+    });
+
+    it("renders truncated product name if it's too long", () => {
+        const longProductName = "This is a very long product name that should be truncated";
+        render(
+            <MemoryRouter>
+                <ProductCard {...defaultProps} productName={longProductName} />
+            </MemoryRouter>
+        );
+        const truncatedProductName = screen.getByText(longProductName.substring(0, 25) + "...");
+        expect(truncatedProductName).toBeInTheDocument();
+    });
+});
