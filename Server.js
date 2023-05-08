faa1f2f8f26b01c56e0b1e1e70a04fc0ea955620
Praise-G <emenikepraise@gmail.com>
2023-05-08 09:17:03 +0200

    Removed unnecessary code

--- Server.js
+++ Server.js
@@ -23,19 +23,12 @@ setTimeout(() => {
 // Setup Stripe
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
-// This is the list of items we are selling
-// This will most likely come from a database or JSON file
-const storeItems = new Map([
-    [1, { priceInCents: 10000, name: "Learn React Today" }],
-    [2, { priceInCents: 15000, name: "Learn CSS Today" }],
-])
 
 app.post('/create-checkout-session', async (req, res)=> {
     try { const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         mode: "payment",
         line_items: req.body.items.map(item => {
-            const storeItem = storeItems.get(item.id)
             return {
                 price_data: {
                     currency: "ZAR",
