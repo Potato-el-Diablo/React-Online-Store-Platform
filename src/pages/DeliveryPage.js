b196e71a8c4da1e81ded4a756d6492d9bf0c94fb
raees-wits <2322203@students.wits.ac.za>
2023-05-11 23:15:19 +0200

    Delivery page?

--- src/pages/DeliveryPage.js
+++ src/pages/DeliveryPage.js
@@ -1,18 +1,20 @@
-//test upload
-import React, { useState, useEffect } from 'react';
-import {collection, getDocs, query, where} from "firebase/firestore";
-import { auth } from "./firebase";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useState } from 'react';
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase";
 import Meta from '../components/Meta';
 import BreadCrumb from '../components/BreadCrumb';
 import { Link } from 'react-router-dom';
-import { db } from "./firebase";
-
-
 
 const DeliveryPage = () => {
   const [deliveryOption, setDeliveryOption] = useState(null);
-  const [deliveryAddress, setDeliveryAddress] = useState('');
+  const [deliveryAddress, setDeliveryAddress] = useState({
+    houseNumber: '',
+    streetName: '',
+    suburb: '',
+    city: '',
+    postalCode: '',
+    collection: ''
+  });
   const [deliveryCost, setDeliveryCost] = useState(null);
   const [deliveryDate, setDeliveryDate] = useState(null);
 
@@ -21,38 +23,68 @@ const DeliveryPage = () => {
   };
 
   const handleDeliveryAddressChange = (event) => {
-    setDeliveryAddress(event.target.value);
+    const { name, value } = event.target;
+    setDeliveryAddress(prevState => ({ ...prevState, [name]: value }));
   };
 
   const handleSubmitAddress = () => {
-    // Set delivery cost and estimated delivery date based on address
-    // This is just a placeholder, in reality this would likely be calculated based on a shipping API
     setDeliveryCost(150);
     const now = new Date();
     const randomInteger = Math.floor(Math.random() * 5) + 1;
     const deliveryDate = new Date(now.getTime() + (randomInteger * 24 * 60 * 60 * 1000));
     setDeliveryDate(deliveryDate.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }));
-   
   };
+
   const [estimatedTime, setEstimatedTime] = useState('2-3 days');
 
-    function handleCollectionCenterChange(value) {
+  const handleCollectionCenterChange = (value) => {
     if (value === 'wits') {
-        setEstimatedTime('1-2 days');
+      setEstimatedTime('1-2 days');
     } else if (value === 'field') {
-        setEstimatedTime('3-4 days');
+      setEstimatedTime('3-4 days');
     } else if (value === 'orange') {
-        setEstimatedTime('2-5 days');
+      setEstimatedTime('2-5 days');
     }
-}
-
-
-  const handleProceedToCheckout = () => {
-    // Redirect to payments portal
-    // This is just a placeholder, in reality you would likely use a router to navigate to the payments page
-    console.log('Proceeding to checkout');
   };
 
+  const handleProceedToCheckout = () => {
+    if (deliveryOption === 'delivery' && deliveryAddress !== '') {
+      console.log('Proceeding to checkout');
+    } else if (deliveryOption === 'collection' && estimatedTime !== '') {
+      console.log('Proceeding to checkout');
+    } else {
+      console.log('Please submit delivery address or select a collection center before proceeding to checkout');
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    if (!deliveryAddress.houseNumber || !deliveryAddress.streetName || !deliveryAddress.suburb || !deliveryAddress.city || !deliveryAddress.postalCode) {
+      alert('Please fill out all fields!');
+      return;
+    }
+
+    try {
+      const docRef = await addDoc(collection(db, 'Orders'), {
+        deliveryAddress: {
+          houseNumber: deliveryAddress.houseNumber,
+          streetName: deliveryAddress.streetName,
+          suburb: deliveryAddress.suburb,
+          city: deliveryAddress.city,
+          postalCode: deliveryAddress.postalCode,
+          collection_center: deliveryAddress.collection,
+        },
+        // other fields for the order
+      });
+      console.log('Order submitted successfully!', docRef.id);
+      alert('Order submitted successfully!');
+    } catch (error) {
+      console.error('Error writing order to Firestore: ', error);
+      alert('Error submitting order. Please try again later.');
+    }
+  };
+
+  
   return (
     <div align="center">
       <h2 className="section-heading" align="center">Choose a delivery option</h2>
@@ -72,7 +104,7 @@ const DeliveryPage = () => {
   <table>
     <tbody>
       <tr>
-        <td><label className='form-input' htmlFor="houseNumber">House/Apartment Number:</label></td>
+        <td><label id="address" className='form-input' htmlFor="houseNumber">House/Apartment Number:</label></td>
         <td><textarea className="form-container" id="houseNumber" name="houseNumber" value={deliveryAddress.houseNumber} onChange={handleDeliveryAddressChange} /></td>
       </tr>
       <tr>
@@ -93,7 +125,7 @@ const DeliveryPage = () => {
       </tr>
     </tbody>
   </table>
-  <button className="form-submit" onClick={handleSubmitAddress}>Submit Address</button>
+  <button className="form-submit" onClick={(e) => handleSubmitAddress(e.target.value)}>Submit Address</button>
 </div>
 
 
@@ -108,7 +140,7 @@ const DeliveryPage = () => {
       {deliveryOption === 'collection' && (
         <div>
             <h3 className="section-heading">Collection Centres</h3>
-            <select onChange={(e) => handleCollectionCenterChange(e.target.value)}>
+            <select id="collection_center"onChange={(e) => handleCollectionCenterChange(e.target.value)}>
             <option value="wits">Wits Pickup Centre</option>
             <option value="field">Field Pickup Centre</option>
             <option value="orange">Orange Pickup Centre</option>
@@ -118,9 +150,13 @@ const DeliveryPage = () => {
     )}
 
       
-      <button class="checkout-button"><b>Proceed to Checkout</b></button>
+{(deliveryOption === 'delivery' && deliveryAddress !== '') || (deliveryOption === 'collection' && estimatedTime !== '' )? (
+  <button id="checkout-btn" className="form-submit" onClick={handleProceedToCheckout}>Proceed to Checkout</button>
+) : null}
+
 
     </div>
+    
     
   );
 };
