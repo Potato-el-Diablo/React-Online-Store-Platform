7d1a5c92f6e725f32f3f6fd4090aca0b94b998dc
neo-nkosi <127707311+neo-nkosi@users.noreply.github.com>
2023-05-14 09:03:57 +0200

    updated OrderDetails.js

--- src/pages/OrderDetails.js
+++ src/pages/OrderDetails.js
@@ -10,6 +10,10 @@ const OrderDetails = () => {
     const { orderNumber } = useParams();
     const [orderDetails, setOrderDetails] = useState(null);
 
+    const calculateSubTotal = (items) => {
+        return items.reduce((total, item) => total + (item.price * item.quantity), 0);
+    };
+
     useEffect(() => {
 
         const fetchOrderDetails = async () => {
@@ -40,10 +44,10 @@ const OrderDetails = () => {
             <div className="store-wrapper home-wrapper-2 py-5">
                 <div className="row">
                     <div className="col-9">
-                        <h2>Order Details</h2>
-                        <h3>Order Number: {orderDetails.orderNumber}</h3>
-                        <p>Order Date: {orderDetails.createdAt.toDate().toLocaleDateString()}</p>
-                        <div className="orderItems">
+                        <h2 style={{textAlign: "center"}}>Order Details</h2>
+                        <h3 style={{textAlign: "center"}}>Order Number: {orderDetails.orderNumber}</h3>
+                        <p style={{textAlign: "center", marginBottom: '30px'}}>Order Date: {orderDetails.createdAt.toDate().toLocaleDateString()}</p>
+                        <div className="orderItems justify-content-center">
                             <div className="d-flex gap-10 flex-wrap">
                                 {orderDetails.items.map((item) => (
                                     <OrderProductCard
@@ -56,6 +60,7 @@ const OrderDetails = () => {
                                 ))}
                             </div>
                         </div>
+                        <h3 style={{textAlign: "center", marginTop: '30px'}}>Subtotal: R{calculateSubTotal(orderDetails.items).toFixed(2)}</h3>
                     </div>
                 </div>
             </div>
