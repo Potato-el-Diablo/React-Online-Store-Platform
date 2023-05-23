c48d6568c52517dde254b6ade312d4bb43632ed4
Joshuaj0209 <joshuaj0209@gmail.com>
2023-05-23 20:06:36 +0200

    Fixed floating footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,15 +2,18 @@ import React from "react";
 import {Outlet} from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import "../App.css";
 
 const Layout = () => {
-    return <>
-        <Header data-testid="header" />
-        <Outlet data-testid="outlet" />
-        <Footer data-testid="footer" />
-
-
-    </>
+    return (
+        <div className="layout">
+            <Header data-testid="header" />
+            <div className="content">
+                <Outlet data-testid="outlet" />
+            </div>
+            <Footer data-testid="footer" />
+        </div>
+    );
 };
 
 export default Layout;
