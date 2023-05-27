09285032a6a4020db9d68d4aa16e927a628c8363
Praise-G <emenikepraise@gmail.com>
2023-05-27 18:26:15 +0200

    Added more test for SingleProduct.test.js

--- src/pages/SingleProduct.test.js
+++ src/pages/SingleProduct.test.js
@@ -18,6 +18,20 @@ const MockLocation = {
     state: MockProduct,
 };
 
+const mockUserId = '12345';
+const mockHandleAddToCart = jest.fn();
+const mockAddReview = jest.fn();
+
+jest.mock('./useCart', () => ({
+    useCart: () => ({ handleAddToCart: mockHandleAddToCart }),
+}));
+
+jest.mock('@firebase/auth', () => ({
+    getAuth: () => ({
+        currentUser: { uid: mockUserId },
+    }),
+}));
+
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useLocation: () => (MockLocation),
@@ -64,6 +78,46 @@ describe('SingleProduct Component', () => {
     test('renders Add to Wishlist link', () => {
         const addToWishlistLink = screen.getByText('Add to Wishlist');
         expect(addToWishlistLink).toBeInTheDocument();
+    });
+
+    test('renders Add Review form if user is logged in', () => {
+        const ratingInput = screen.getByLabelText('Rating');
+        const commentInput = screen.getByLabelText('Review');
+        const submitButton = screen.getByText('Submit Review');
+        expect(ratingInput).toBeInTheDocument();
+        expect(commentInput).toBeInTheDocument();
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.change(ratingInput, { target: { value: 4 } });
+        fireEvent.change(commentInput, { target: { value: 'Great product!' } });
+        fireEvent.click(submitButton);
+
+        expect(mockAddReview).toHaveBeenCalledWith('12345', 'Great product!', 4);
+    });
+
+    test('renders average rating', () => {
+        const avgRating = screen.getByText('Average Rating:');
+        expect(avgRating).toBeInTheDocument();
+    });
+
+    test('renders rating stars correctly', () => {
+        const ratingStars = screen.getAllByRole('img', { name: /star/i });
+        expect(ratingStars.length).toBe(5);
+    });
+
+    test('calls handleAddToCart when Add to Cart button is clicked', () => {
+        const addToCartButton = screen.getByText('Add to Cart');
+        fireEvent.click(addToCartButton);
+        expect(mockHandleAddToCart).toHaveBeenCalledWith(MockProduct);
+    });
+
+    test('matches snapshot', () => {
+        const { asFragment } = render(
+            <BrowserRouter>
+                <SingleProduct />
+            </BrowserRouter>
+        );
+        expect(asFragment()).toMatchSnapshot();
     });
 
 
