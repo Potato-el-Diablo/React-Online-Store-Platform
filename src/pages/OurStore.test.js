8f2be6fc07562984745dfa3e1095943f29bf2a8e
raees-wits <2322203@students.wits.ac.za>
2023-05-28 22:36:33 +0200

    Our store Unit test

--- src/pages/OurStore.test.js
+++ src/pages/OurStore.test.js
@@ -36,4 +36,51 @@ describe('OurStore Component', () => {
         expect(totalProductsText).toBeInTheDocument();
     });
 
+    test('renders product cards with correct data', () => {
+        // Mock the data for product cards
+        const products = [
+            {
+                id: '1',
+                image: 'product1.jpg',
+                brand: 'Brand 1',
+                name: 'Product 1',
+                description: 'Description 1',
+                price: 100,
+                sale: '',
+                category: 'Category 1',
+                stock: 10,
+                averageRating: 4.5
+            },
+            {
+                id: '2',
+                image: 'product2.jpg',
+                brand: 'Brand 2',
+                name: 'Product 2',
+                description: 'Description 2',
+                price: 200,
+                sale: 'Sale 2',
+                category: 'Category 2',
+                stock: 5,
+                averageRating: 3.8
+            }
+        ];
+
+        render(
+            <BrowserRouter>
+                <OurStore />
+            </BrowserRouter>
+        );
+
+        // Mock the products state to be used within the component
+        jest.spyOn(React, 'useState').mockImplementation(() => [products, jest.fn()]);
+
+        // Assert that the product cards are rendered with correct data
+        const productCard1 = screen.getByText('Product 1');
+        expect(productCard1).toBeInTheDocument();
+        // Add more assertions for the other properties of productCard1
+
+        const productCard2 = screen.getByText('Product 2');
+        expect(productCard2).toBeInTheDocument();
+        // Add more assertions for the other properties of productCard2
+    });
 });
