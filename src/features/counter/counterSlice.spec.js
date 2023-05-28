0f0337e9c78b03ec0824420820dff918b069e299
neo-nkosi <127707311+neo-nkosi@users.noreply.github.com>
2023-05-28 23:08:18 +0200

    Updated counterSlice.spec.js

--- src/features/counter/counterSlice.spec.js
+++ src/features/counter/counterSlice.spec.js
@@ -1,17 +1,29 @@
 import counterReducer, {
   increment,
   decrement,
-  incrementByAmount,
+  incrementByAmount, incrementIfOdd, selectCount, incrementAsync,
 } from './counterSlice';
+import {configureStore} from "@reduxjs/toolkit";
+import { fetchCount } from './counterAPI';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureMockStore(middlewares)
+
+
+// For async actions
+jest.mock('./counterAPI');
 
 describe('counter reducer', () => {
   const initialState = {
     value: 3,
     status: 'idle',
   };
+
   it('should handle initial state', () => {
-    expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
-      value: 0,
+    expect(counterReducer(undefined, {type: 'unknown'})).toEqual({
+      value: 11,
       status: 'idle',
     });
   });
@@ -29,5 +41,36 @@ describe('counter reducer', () => {
   it('should handle incrementByAmount', () => {
     const actual = counterReducer(initialState, incrementByAmount(2));
     expect(actual.value).toEqual(5);
+  });
+
+  it('should handle incrementAsync', async () => {
+    fetchCount.mockResolvedValueOnce({data: 5});
+    const expectedActions = [
+      {type: 'counter/fetchCount/pending'},
+      {type: 'counter/fetchCount/fulfilled', payload: 5},
+    ];
+    const store = mockStore({counter: initialState});
+    await store.dispatch(incrementAsync(5));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('should select count', () => {
+    const state = {counter: {value: 5}};
+    const res = selectCount(state);
+    expect(res).toEqual(5);
+  });
+
+  it('should increment if odd', () => {
+    const store = mockStore({counter: initialState});
+    store.dispatch(incrementIfOdd(5));
+    const actions = store.getActions();
+    expect(actions).toEqual([incrementByAmount(5)]);
+  });
+
+  it('should not increment if even', () => {
+    const store = mockStore({counter: {value: 4}});
+    store.dispatch(incrementIfOdd(5));
+    const actions = store.getActions();
+    expect(actions).toEqual([]);
   });
 });
